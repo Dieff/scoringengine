fb770581593b36ff1a07df2975f83e7ece4c12ea
Admin I strator <prepadmin@scoring0.edcp.spicelab.org>
2021-01-03 16:07:37 -0500

    getting things running

--- docker/wait-for-container.sh
+++ docker/wait-for-container.sh
@@ -6,11 +6,7 @@ host="$1"
 shift
 cmd=("$@")
 
-while ping $host -c 1 > /dev/null 2>&1
-do
-  >&2 echo "$host container is still running...sleeping 15 seconds"
-  sleep 15
-done
+sleep 45
 
 >&2 echo "$host container is finished...starting container"
 exec "${cmd[@]}"
